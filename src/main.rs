e8bb4cf0371013c2f36f289de86e87b1fda868c8
Vincent Stakenburg <vincentstakenburg@gmail.com>
2018-11-07 01:03:46 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+// panic-handler crate
+extern crate panic_semihosting;
+//#[macro_use] extern crate cortex_m;
+
+extern crate rtfm;
+
+//use stm32l432xx_hal;
+
+use rtfm::app;
+
+//use rtfm::{P0, T0, TMax};
+
+//extern crate stm32l4;/**/
+
+//use stm32l4;
+
+extern crate stm32l4;
+
+//use stm32l4::stm32l4x2::{Peripherals};
+//use stm32l4::stm32l4x2::*;
+
+//use cortex_m::asm;
+//use core::fmt::Write;
+#[macro_use] extern crate cortex_m_semihosting;
+
+#[app(device = stm32l4::stm32l4x2)]
+const APP: () = {
+
+
+    #[init]
+    fn init() {
+        let per: stm32l4::stm32l4x2::Peripherals = device;
+        let rcc: &stm32l4::stm32l4x2::RCC = &per.RCC;
+        rcc.ahb2enr.write(|w| {
+           w.gpioben().set_bit()
+        });
+        let gpiob = &per.GPIOB;
+        gpiob.pupdr.write(|w| {
+            w.pupdr3().pull_down()
+        });
+        gpiob.moder.write(|w| {
+            w.moder3().output()
+        });
+//        gpiob.odr.write(|w| {
+//            w.odr3().high()
+//        });
+        hprintln!("aa").unwrap();
+        loop {
+            hprintln!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").unwrap();
+            gpiob.odr.write(|w| {
+                w.odr3().bit(gpiob.odr.read().odr3().bit_is_clear())
+            });
+            hprintln!("!").unwrap();
+        }
+
+    }
+};
