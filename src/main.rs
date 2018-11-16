5eed2dbac992c5f5e39ffefa370f8b5127f82eb9
Vincent Stakenburg <vincentstakenburg@gmail.com>
2018-11-17 00:19:25 +0100

    things

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![deny(warnings)]
 #![no_main]
 #![no_std]
@@ -18,15 +17,12 @@ use rtfm::app;
 
 //extern crate stm32l4;/**/
 
+
+use cortex_m::asm::delay;
 //use stm32l4;
 
 extern crate stm32l4;
 
-//use stm32l4::stm32l4x2::{Peripherals};
-//use stm32l4::stm32l4x2::*;
-
-//use cortex_m::asm;
-//use core::fmt::Write;
 #[macro_use] extern crate cortex_m_semihosting;
 
 #[app(device = stm32l4::stm32l4x2)]
@@ -37,9 +33,15 @@ const APP: () = {
     fn init() {
         let per: stm32l4::stm32l4x2::Peripherals = device;
         let rcc: &stm32l4::stm32l4x2::RCC = &per.RCC;
+        per.LPUART1
         rcc.ahb2enr.write(|w| {
            w.gpioben().set_bit()
         });
+        unsafe {
+            rcc.cfgr.write(|w| {
+                w.sw().bits(0b01)
+            });
+        }
         let gpiob = &per.GPIOB;
         gpiob.pupdr.write(|w| {
             w.pupdr3().pull_down()
@@ -52,11 +54,14 @@ const APP: () = {
 //        });
         hprintln!("aa").unwrap();
         loop {
-            hprintln!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").unwrap();
+
+            hprintln!("{:?}", rcc.cfgr.read().sw().bits()).unwrap();
+//            hprintln!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").unwrap();
             gpiob.odr.write(|w| {
                 w.odr3().bit(gpiob.odr.read().odr3().bit_is_clear())
             });
-            hprintln!("!").unwrap();
+            delay(1000000);
+//            hprintln!("!").unwrap();
         }
 
     }
